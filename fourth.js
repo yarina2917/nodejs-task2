11849c757b42b4b28723f30d5623f96bc110cb6d
yarina2917 <yarina297@gmail.com>
2019-10-21 00:15:31 +0300

    fixed fourt task

--- fourth.js
+++ fourth.js
@@ -1,37 +1,20 @@
 // Create function with 2 string params. It must check if letters in the first correspond to the number of matches in second and return %.
 
 function compareStrings(str1, str2) {
-    let count = {};
-    let check = false;
+    let count = 0;
+    let str1Arr = str1.split('');
+    let str2Arr = str2.split('');
 
-    for (let i = 0; i < str1.length; i++) {
-        for (let y = 0; y < str2.length; y++) {
-            if (str1[i] === str2[y]) {
-                let letter = str1[i];
-                if (!count[letter]) {
-                    // if no letter in obj, write it, array - [count of letter from first str, count of letter from second str]
-                    // check variable uses for writing all count of letter from second string, when we find same letters in first time
-                    count[letter] = [1, 1];
-                    check = true;
-                } else if (count[letter][0] === 1 && check) {
-                    // write all count of letter from second string
-                    count[letter][1]++;
-                } else {
-                    // break, when we find same letter in first string not in first time
-                    count[letter][0]++;
-                    break
-                }
+    for (let i = 0; i < str1Arr.length; i++) {
+        for (let y = 0; y < str2Arr.length; y++) {
+            if (str1Arr[i] === str2Arr[y]) {
+                count++;
+                str2Arr.splice(y, 1);
+                break
             }
         }
-        check = false;
     }
-
-    // find sum of letters obj
-    let sum = 0;
-    for (let value in count) {
-        sum += Math.min(...count[value]);
-    }
-    return (sum * 100 / Math.min(str1.length, str2.length)).toFixed(2) + '%';
+    return (count * 2 / (str1.length + str2.length)).toFixed(2) + '%';
 }
 
 console.log(compareStrings('qqwerty', 'qqqwww'));
